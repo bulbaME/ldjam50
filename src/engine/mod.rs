ebdd78a98c2a947be08cc7d74a887e04631ea614
DAk001 <def.fed2006@gmail.com>
2022-04-04 00:37:07 +0300

    Add main menu

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -38,7 +38,7 @@ pub type Size = Vector2<f32>;
 static NO_SIZE: Vector2<f32> = Vector2 {x: 0.0, y: 0.0};
 static NO_POS: Vector3<f32> = Vector3 {x: 0.0, y: 0.0, z: 0.0};
 
-static WINDOW_WIDTH: u32 = 800;
+static WINDOW_WIDTH: u32 = 1200;
 static WINDOW_HEIGHT: u32 = 800;
 static WINDOW_TITLE: &str = "LD50 Game";
 
